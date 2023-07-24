caf0cb831bf6ddb8aa0e05b6a63de1f1b61c34a1
imene <106630138+imeneazouzi@users.noreply.github.com>
2023-07-24 12:54:39 +0100

    removing set and get attributes from ParametersServiceImpl

--- src/main/java/com/elyadata/webResumeGenerator/services/impl/ParameterServiceImpl.java
+++ src/main/java/com/elyadata/webResumeGenerator/services/impl/ParameterServiceImpl.java
@@ -1,4 +1,5 @@
 package com.elyadata.webResumeGenerator.services.impl;
+
 import com.elyadata.webResumeGenerator.dto.ParametersDTO;
 import com.elyadata.webResumeGenerator.execption.NotFoundException;
 import com.elyadata.webResumeGenerator.mapper.ParametersMapper;
@@ -6,6 +7,7 @@ import com.elyadata.webResumeGenerator.model.Parameters;
 import com.elyadata.webResumeGenerator.repo.ParametersRepository;
 import com.elyadata.webResumeGenerator.services.ParametersService;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -13,35 +15,34 @@ public class ParameterServiceImpl implements ParametersService {
 
     private final ParametersRepository parametersRepository;
     private final ParametersMapper parametersMapper;
-    public ParameterServiceImpl (ParametersRepository parametersRepository ,ParametersMapper parametersMapper)
-    {
-        this. parametersRepository =  parametersRepository;
+
+    public ParameterServiceImpl(ParametersRepository parametersRepository, ParametersMapper parametersMapper) {
+        this.parametersRepository = parametersRepository;
         this.parametersMapper = parametersMapper;
     }
+
     @Override
     public ParametersDTO addParameters(ParametersDTO parametersDto) {
         return parametersMapper.toDto(parametersRepository.save(parametersMapper.toEntity(parametersDto)));
     }
+
     @Override
     public List<ParametersDTO> findAllParameters() {
         return parametersMapper.toDto(parametersRepository.findAll());
     }
+
     @Override
-    public void deleteParameters(Long id){
+    public void deleteParameters(Long id) {
         parametersRepository.deleteById(id);
     }
+
     @Override
-    public ParametersDTO updateParameters(ParametersDTO parametersDto){
+    public ParametersDTO updateParameters(ParametersDTO parametersDto) {
         Parameters existingParameters = parametersRepository.findById(parametersDto.getId())
                 .orElseThrow(() -> new IllegalArgumentException("Parameter not found with ID: " + parametersDto.getId()));
-        existingParameters.setTitle(parametersDto.getTitle());
-        existingParameters.setDescription(parametersDto.getDescription());
-        existingParameters.setStartDate(parametersDto.getStartDate());
-        existingParameters.setEndDate(parametersDto.getEndDate());
-        existingParameters.setPlace(parametersDto.getPlace());
-        existingParameters.setSummary(parametersDto.getSummary());
         return parametersMapper.toDto(parametersRepository.save(existingParameters));
     }
+
     @Override
     public ParametersDTO findParametersById(Long id) {
         return parametersMapper.toDto(parametersRepository.findById(id)
